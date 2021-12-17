078f08a97f38727d96355244a599ccb2d4f14bad
Naurotski <6602796@gmail.com>
2021-12-17 22:09:04 +0100

    ServisWorker ...

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'MetaMorfosi',
+    themeColor: '#FF9800',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'public/service-worker.js'
+    }
+  }
 }
