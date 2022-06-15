58c7b08cf1f731c1808d625e3c308eac3dd0b859
Naurotski <6602796@gmail.com>
2022-06-15 21:56:48 +0200

    Filter.vue

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -6,19 +6,25 @@
       </v-btn>
     </div>
     <v-expand-transition>
-      <div v-if="showTabs" class="text-center">
-        <v-btn
-          v-for="(tab, i) in categories"
-          :key="i"
-          text
-          @click="changeCategory(tab)"
-          :class="{ primary: tab === filter }"
-          class="ml-1"
-        >
-          {{ tab }}
-        </v-btn>
-      </div>
+      <v-tabs v-model="tab" v-if="showTabs" value="3" centered show-arrows center-active>
+        <v-tab v-for="{ category } in categories" :key="category" @click="changeCategory(category)">
+          {{ category }}
+        </v-tab>
+      </v-tabs>
     </v-expand-transition>
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="{ category, description } in categories" :key="description">
+        <section key="a" id="intro" class="text-center mb-12 mx-3">
+          <h1 class="display-2 font-weight-light mb-8">{{ category }}</h1>
+          <v-responsive
+            style="white-space: pre-line"
+            class="mx-auto subtitle-1 grey--text"
+            max-width="750"
+            v-text="description"
+          />
+        </section>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
@@ -28,11 +34,14 @@ export default {
   name: 'VFilter',
 
   data: () => ({
-    showTabs: true
+    showTabs: true,
+    tab: null
   }),
-
   computed: {
     ...mapState(['categories', 'filter'])
+  },
+  mounted() {
+    this.tab = this.categories.findIndex((item) => item.category === this.filter)
   },
   methods: {
     changeCategory(category) {
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,47 +1,7 @@
 <template>
-  <v-container id="home" class="pt-12 pb-0 px-0" fluid tag="section">
+  <v-container id="home" class="pb-0 px-0" fluid tag="section">
     <transition-group appear name="fadeGroup" mode="out-in">
       <v-filter key="abc" />
-      <section key="a" id="intro" class="text-center mb-12 mx-3">
-        <h1 class="display-2 font-weight-light mb-8">{{ filter }}</h1>
-        <v-responsive
-          v-if="filter === 'Visible World'"
-          class="mx-auto subtitle-1 grey--text"
-          max-width="750"
-        >
-          Here you can see the artworks from different years. In each painting, there is a visible
-          world. But behind each image, there is something more than can meet your eyes. Each canvas
-          is a transition to a point between the worlds where boundaries are erased and narratives
-          dissolve. The viewer immerses into his own unconscious, into a state of "here and
-          nowhere." This is the experience of living free from any restrictions.
-        </v-responsive>
-        <v-responsive
-          v-if="filter === 'With a Secret'"
-          class="mx-auto subtitle-1 grey--text"
-          max-width="750"
-        >
-          These paintings have two sides. The front side is a familiar landscape or portrait. But
-          there is always a little window with glass. Which you can look into. And see what is
-          there. On the other side... Only the owner of the painting knows what's on the back. This
-          is their personal secret. <br />
-          <br />
-          Touch the cut on the canvas, touch the piece of stained glass, turn the canvas over. One
-          small action and you will be beyond the visible reality. The collection "With a Secret"
-          includes the artist's works, created at the intersection of techniques and practices. This
-          not only enriches the visual language, but it also gives everyone their own unique
-          experience of interactive engagement with the painting.
-        </v-responsive>
-        <v-responsive
-          v-if="filter === 'Objects'"
-          class="mx-auto subtitle-1 grey--text"
-          max-width="750"
-        >
-          The author goes beyond the canvas. Stained-glass artworks and mosaics, mosaic sculptures
-          become independent art objects. They tell more than pictures, but remain in the same
-          paradigm: they allow you to see hidden meanings. It’s only this time when the volume
-          allows you to see much more than the canvas does.
-        </v-responsive>
-      </section>
       <Paintings key="b" :listPictures="sortPaintings" />
     </transition-group>
   </v-container>
